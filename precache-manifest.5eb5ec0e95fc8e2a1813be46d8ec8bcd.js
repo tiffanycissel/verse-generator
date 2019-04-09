9113b0988e866bdf2e8e6201957cd310392e2f85
Tiffany Cissel <tiffany.cissel@gmail.com>
2019-04-09 14:33:14 -0400

    Updates

---
+++ precache-manifest.5eb5ec0e95fc8e2a1813be46d8ec8bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "817c6fc23cce19fc7596e84249358996",
+    "url": "/verse-generator/static/media/baseline-launch-24px_no-pad.817c6fc2.svg"
+  },
+  {
+    "revision": "af598a7af68c2b835617",
+    "url": "/verse-generator/static/js/runtime~main.2e00f91a.js"
+  },
+  {
+    "revision": "b027c74d97394afee593",
+    "url": "/verse-generator/static/js/main.a582e2e0.chunk.js"
+  },
+  {
+    "revision": "0a0164646a76fd825593",
+    "url": "/verse-generator/static/js/2.a8a9e98d.chunk.js"
+  },
+  {
+    "revision": "b027c74d97394afee593",
+    "url": "/verse-generator/static/css/main.1fc824a3.chunk.css"
+  },
+  {
+    "revision": "789c778261401130f8fea5af239037f8",
+    "url": "/verse-generator/index.html"
+  }
+];
